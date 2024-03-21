201203bf89f5faab4b2b73f3fca88bec0d60ee65
Srinu363 <geddadasri872@gmail.com>
2024-03-21 11:15:08 +0530

    input file is added

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class input{
+public static void main(String Args[]){
+Scanner scanner = new Scanner(System.in);System.out.print("enter the name");String name = scanner.nextLine();
+System.out.print(name);
+scanner.close();
+
+    }
+}
