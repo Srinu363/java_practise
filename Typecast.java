5d3cf7ab7ca7e0b0ff123b632610b7c499e33c88
Srinu363 <geddadasri872@gmail.com>
2024-03-25 12:32:21 +0530

    files are added

---
+++ Typecast.java
@@ -0,0 +1,14 @@
+// converting to lager to small or small to larger 
+class Typecast{
+    public static void main(String e[]){
+        byte a = 10;
+        double b = a;
+        short c = 65;
+        char d = (char) c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
